19c8be8f845be71d284d470bee3d91a7bb6fd650
Martin Joshua Garaza <57517246+stronze21@users.noreply.github.com>
2026-01-05 09:25:03 +0800

    Update AgreementView.php

--- app/Http/Livewire/Orders/AgreementView.php
+++ app/Http/Livewire/Orders/AgreementView.php
@@ -45,7 +45,7 @@ class AgreementView extends Component
         $products = Product::all();
         $payments = OrderPaymentHistory::where('oa_id', $this->oa->oa_id)->latest('date_of_payment')->get();
         $initial = OrderPaymentHistory::where('oa_id', $this->oa->oa_id)->orderBy('date_of_payment')->first();
-        $total_paid = OrderPaymentHistory::where('oa_id', $this->oa->oa_id)->sum('amount');
+        $total_paid = OrderPaymentHistory::where('oa_id', $this->oa->oa_id)->whereNot('status', 'Voided')->sum('amount');
         $mops = ModeOfPayment::all();
 
         return view('livewire.orders.agreement-view', [
